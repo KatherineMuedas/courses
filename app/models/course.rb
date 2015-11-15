d757a3e48e91d074207e7f6e8c065cb4b5617eef
katherine <muedasnka@gmail.com>
2015-11-14 23:54:10 -0700

    api call in course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,10 @@
 class Course
-  
+  include HTTParty
+  base_uri 'https://api.coursera.org/api/catalog.v1/courses'
+  default_params fields: "smallIcom, shortDescription" , q: "search"
+  format :json
+
+  def self.for term
+    get("",query:{ query: term})["elements"]
+  end
 end
